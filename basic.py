11db8fdd6879c537905dcac430ee2f367e6e99f2
mridultuteja <mridul2507@gmail.com>
2019-03-29 08:15:53 +0530

    updated file

--- basic.py
+++ basic.py
@@ -1,68 +1,96 @@
-import cv2, time, pandas 
-from datetime import datetime 
-static_back = None
-motion_list = [ None, None ] 
-time = [] 
-df = pandas.DataFrame(columns = ["Start", "End"]) 
-video = cv2.VideoCapture(0) 
-while True: 
-    check, frame = video.read() 
+# USAGE
+# python motion_detector.py
+# python motion_detector.py --video videos/example_01.mp4
 
-    motion = 0
+# import the necessary packages
+from imutils.video import VideoStream
+import argparse
+import datetime
+import imutils
+import time
+import cv2
 
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video", help="path to the video file")
+ap.add_argument("-a", "--min-area", type=int, default=500, help="minimum area size")
+args = vars(ap.parse_args())
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
-    gray = cv2.GaussianBlur(gray, (21, 21), 0) 
-    if static_back is None: 
-        static_back = gray 
-        continue
-    diff_frame = cv2.absdiff(static_back, gray) 
-    thresh_frame = cv2.threshold(diff_frame, 30, 255, cv2.THRESH_BINARY)[1] 
-    thresh_frame = cv2.dilate(thresh_frame, None, iterations = 2) 
-    (_, cnts, _) = cv2.findContours(thresh_frame.copy(), 
-                    cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) 
+# if the video argument is None, then we are reading from webcam
+if args.get("video", None) is None:
+	vs = VideoStream(src=0).start()
+	time.sleep(2.0)
 
-    for contour in cnts: 
-        if cv2.contourArea(contour) < 10000: 
-            continue
-        motion = 1
+# otherwise, we are reading from a video file
+else:
+	vs = cv2.VideoCapture(args["video"])
 
-        (x, y, w, h) = cv2.boundingRect(contour) 
+# initialize the first frame in the video stream
+firstFrame = None
 
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3) 
-    motion_list.append(motion) 
+# loop over the frames of the video
+while True:
+	# grab the current frame and initialize the occupied/unoccupied
+	# text
+	frame = vs.read()
+	frame = frame if args.get("video", None) is None else frame[1]
+	text = "Unoccupied"
 
-    motion_list = motion_list[-2:] 
+	# if the frame could not be grabbed, then we have reached the end
+	# of the video
+	if frame is None:
+		break
 
+	# resize the frame, convert it to grayscale, and blur it
+	frame = imutils.resize(frame, width=500)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
-    if motion_list[-1] == 1 and motion_list[-2] == 0: 
-        time.append(datetime.now()) 
+	# if the first frame is None, initialize it
+	if firstFrame is None:
+		firstFrame = gray
+		continue
 
+	# compute the absolute difference between the current frame and
+	# first frame
+	frameDelta = cv2.absdiff(firstFrame, gray)
+	thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
 
-    if motion_list[-1] == 0 and motion_list[-2] == 1: 
-        time.append(datetime.now()) 
+	# dilate the thresholded image to fill in holes, then find contours
+	# on thresholded image
+	thresh = cv2.dilate(thresh, None, iterations=2)
+	cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+		cv2.CHAIN_APPROX_SIMPLE)
+	cnts = imutils.grab_contours(cnts)
 
-    cv2.imshow("Gray Frame", gray) 
+	# loop over the contours
+	for c in cnts:
+		# if the contour is too small, ignore it
+		if cv2.contourArea(c) < args["min_area"]:
+			continue
 
-    cv2.imshow("Difference Frame", diff_frame) 
+		# compute the bounding box for the contour, draw it on the frame,
+		# and update the text
+		(x, y, w, h) = cv2.boundingRect(c)
+		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+		text = "Occupied"
 
-    cv2.imshow("Threshold Frame", thresh_frame) 
-    cv2.imshow("Color Frame", frame) 
+	# draw the text and timestamp on the frame
+	cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
+		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+	cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
+		(10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
 
-    key = cv2.waitKey(1) 
-    if key == ord('q'): 
-        if motion == 1: 
-            time.append(datetime.now()) 
-        break
+	# show the frame and record if the user presses a key
+	cv2.imshow("Security Feed", frame)
+	cv2.imshow("Thresh", thresh)
+	cv2.imshow("Frame Delta", frameDelta)
+	key = cv2.waitKey(1) & 0xFF
 
+	# if the `q` key is pressed, break from the lop
+	if key == ord("q"):
+		break
 
-for i in range(0, len(time), 2): 
-    df = df.append({"Start":time[i], "End":time[i + 1]}, ignore_index = True) 
-
-
-df.to_csv("Time_of_movements.csv") 
-
-video.release() 
-
-cv2.destroyAllWindows() 
-
+# cleanup the camera and close any open windows
+vs.stop() if args.get("video", None) is None else vs.release()
+cv2.destroyAllWindows()
